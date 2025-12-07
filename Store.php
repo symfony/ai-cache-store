714217eafb230ce5b275ec7b7e17fd302972f10a
Oskar Stark <oskarstark@googlemail.com>
2025-12-07 16:41:04 +0100

    [Store] Split Local bridge into Cache and Memory bridges

---
+++ Store.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\Bridge\Cache;
+
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\AI\Platform\Vector\Vector;
+use Symfony\AI\Store\Distance\DistanceCalculator;
+use Symfony\AI\Store\Document\Metadata;
+use Symfony\AI\Store\Document\VectorDocument;
+use Symfony\AI\Store\Exception\InvalidArgumentException;
+use Symfony\AI\Store\Exception\RuntimeException;
+use Symfony\AI\Store\ManagedStoreInterface;
+use Symfony\AI\Store\StoreInterface;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Contracts\Cache\CacheInterface;
+
+/**
+ * @author Guillaume Loulier <personal@guillaumeloulier.fr>
+ */
+final class Store implements ManagedStoreInterface, StoreInterface
+{
+    public function __construct(
+        private readonly CacheInterface&CacheItemPoolInterface $cache,
+        private readonly DistanceCalculator $distanceCalculator = new DistanceCalculator(),
+        private readonly string $cacheKey = '_vectors',
+    ) {
+        if (!interface_exists(CacheInterface::class)) {
+            throw new RuntimeException('For using the Cache store as vector store, a symfony/contracts cache implementation is required. Try running "composer require symfony/cache" or another symfony/contracts compatible cache.');
+        }
+    }
+
+    public function setup(array $options = []): void
+    {
+        if ([] !== $options) {
+            throw new InvalidArgumentException('No supported options.');
+        }
+
+        if ($this->cache->hasItem($this->cacheKey)) {
+            return;
+        }
+
+        $this->cache->get($this->cacheKey, static fn (): array => []);
+    }
+
+    public function add(VectorDocument ...$documents): void
+    {
+        $existingVectors = $this->cache->get($this->cacheKey, static fn (): array => []);
+
+        $newVectors = array_map(static fn (VectorDocument $document): array => [
+            'id' => $document->id->toRfc4122(),
+            'vector' => $document->vector->getData(),
+            'metadata' => $document->metadata->getArrayCopy(),
+        ], $documents);
+
+        $cacheItem = $this->cache->getItem($this->cacheKey);
+
+        $cacheItem->set([
+            ...$existingVectors,
+            ...$newVectors,
+        ]);
+
+        $this->cache->save($cacheItem);
+    }
+
+    /**
+     * @param array{
+     *     maxItems?: positive-int,
+     *     filter?: callable(VectorDocument): bool
+     * } $options If maxItems is provided, only the top N results will be returned.
+     *            If filter is provided, only documents matching the filter will be considered.
+     */
+    public function query(Vector $vector, array $options = []): iterable
+    {
+        $documents = $this->cache->get($this->cacheKey, static fn (): array => []);
+
+        $vectorDocuments = array_map(static fn (array $document): VectorDocument => new VectorDocument(
+            id: Uuid::fromString($document['id']),
+            vector: new Vector($document['vector']),
+            metadata: new Metadata($document['metadata']),
+        ), $documents);
+
+        if (isset($options['filter'])) {
+            $vectorDocuments = array_values(array_filter($vectorDocuments, $options['filter']));
+        }
+
+        yield from $this->distanceCalculator->calculate($vectorDocuments, $vector, $options['maxItems'] ?? null);
+    }
+
+    public function drop(): void
+    {
+        $this->cache->clear();
+    }
+}
